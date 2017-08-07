e0d08db7cd525aeb6260556596a22c48775520e8
luowg <wengang.luo@cocopico.com>
2017-08-07 19:22:45 +0800

    增加功能

--- quantify-economy-service/src/main/java/com/flyonsky/quantify/model/chart/AbstractChartData.java
+++ quantify-economy-service/src/main/java/com/flyonsky/quantify/model/chart/AbstractChartData.java
@@ -17,6 +17,24 @@ public abstract class AbstractChartData {
 		case netprofit:
 			kpiName = "净利润";
 			break;
+		case shnetprofit:
+			kpiName = "归属股东净利润";
+			break;
+		case deshnetprofit:
+			kpiName = "归属股东净利润(扣非)";
+			break;
+		case netcashflow:
+			kpiName="经营活动现金流量净额";
+			break;
+		case totalassets:
+			kpiName="总资产";
+			break;
+		case totalliability:
+			kpiName="总负债";
+			break;
+		case shequity:
+			kpiName="股东权益";
+			break;
 		}
 		return kpiName;
 	}
