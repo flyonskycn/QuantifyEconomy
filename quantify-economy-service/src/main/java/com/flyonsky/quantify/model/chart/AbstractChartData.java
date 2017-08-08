65131f9666b75936a6ad4db4a98649950d8cd30d
luowg <wengang.luo@cocopico.com>
2017-08-08 17:03:31 +0800

    添加功能

--- quantify-economy-service/src/main/java/com/flyonsky/quantify/model/chart/AbstractChartData.java
+++ quantify-economy-service/src/main/java/com/flyonsky/quantify/model/chart/AbstractChartData.java
@@ -1,5 +1,7 @@
 package com.flyonsky.quantify.model.chart;
 
+import com.flyonsky.quantify.model.Description;
+
 public abstract class AbstractChartData {
 
 	/**
@@ -8,108 +10,16 @@ public abstract class AbstractChartData {
 	 * @return
 	 */
 	public String kpiName(EnumKpiType kpiType){
-		String kpiName = null;
-		switch(kpiType){
-		case revenue:
-			kpiName = "营业收入";
-			break;
-		case revenuewith:
-			kpiName = "营业收入(同比)";
-			break;
-		case operprofit:
-			kpiName = "营业利润";
-			break;
-		case operprofitwith:
-			kpiName = "营业利润(同比)";
-			break;
-		case totalprofit:
-			kpiName = "利润总额";
-			break;
-		case totalprofitwith:
-			kpiName = "利润总额(同比)";
-			break;
-		case netprofit:
-			kpiName = "净利润";
-			break;
-		case netprofitwith:
-			kpiName = "净利润(同比)";
-			break;
-		case shnetprofit:
-			kpiName = "归属股东净利润";
-			break;
-		case shnetprofitwith:
-			kpiName = "归属股东净利润(同比)";
-			break;
-		case deshnetprofit:
-			kpiName = "归属股东净利润(扣非)";
-			break;
-		case deshnetprofitwith:
-			kpiName = "扣非股东净利润(同比)";
-			break;
-		case netcashflow:
-			kpiName="经营活动现金流量净额";
-			break;
-		case netcashflowwith:
-			kpiName="经营活动现金流量净额(同比)";
-			break;
-		case totalassets:
-			kpiName="总资产";
-			break;
-		case totalassetswith:
-			kpiName="总资产(同比)";
-			break;
-		case totalliability:
-			kpiName="总负债";
-			break;
-		case totalliabilitywith:
-			kpiName="总负债(同比)";
-			break;
-		case shequity:
-			kpiName="股东权益";
-			break;
-		case shequitywith:
-			kpiName="股东权益(同比)";
-			break;
-		case totalshares:
-			kpiName="股份总数";
-			break;
-		case totalshareswith:
-			kpiName="股份总数(同比)";
-			break;
-		case pershare:
-			kpiName="每股收益";
-			break;
-		case persharewith:
-			kpiName="每股收益(同比)";
-			break;
-		case assetturnover:
-			kpiName="资产周转率";
-			break;
-		case grossmargin:
-			kpiName="毛利率";
-			break;
-		case netinterestrate:
-			kpiName="净利率";
-			break;
-		case mainprofitrate:
-			kpiName="主营业务利润占比";
-			break;
-		case debtratio:
-			kpiName="负债率";
-			break;
-		case netassetspershare:
-			kpiName="每股净资产";
-			break;
-		case totalassetsprofitrate:
-			kpiName="总资产收益率";
-			break;
-		case netAssetsProfitRate:
-			kpiName="净资产收益率";
-			break;
-		case persharecash:
-			kpiName="每股现金净额";
-			break;
+		String kpiName = "";
+		
+		try {
+			Description des = kpiType.getClass().getField(kpiType.name()).getAnnotation(Description.class);
+			kpiName = des.value();
+		} catch (NoSuchFieldException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
 		return kpiName;
 	}
 	
--- quantify-economy-service/src/main/java/com/flyonsky/quantify/model/chart/EnumKpiType.java
+++ quantify-economy-service/src/main/java/com/flyonsky/quantify/model/chart/EnumKpiType.java
@@ -1,5 +1,7 @@
 package com.flyonsky.quantify.model.chart;
 
+import com.flyonsky.quantify.model.Description;
+
 /**
  * 指标类型
  * @author Administrator
@@ -9,133 +11,166 @@ public enum EnumKpiType {
 	/**
 	 * 营业收入
 	 */
+	@Description("营业收入")
 	revenue,
 	/**
 	 * 营业收入(同比)
 	 */
+	@Description("营业收入(同比)")
 	revenuewith,
 	/**
 	 * 营业利润
 	 */
+	@Description("营业利润")
 	operprofit,
 	/**
 	 * 营业利润(同比)
 	 */
+	@Description("营业利润(同比)")
 	operprofitwith,
 	/**
 	 * 利润总额
 	 */
+	@Description("利润总额")
 	totalprofit,
 	/**
 	 * 利润总额(同比)
 	 */
+	@Description("利润总额(同比)")
 	totalprofitwith,
 	/**
 	 * 净利润
 	 */
+	@Description("净利润")
 	netprofit,
 	/**
 	 * 净利润(同比)
 	 */
+	@Description("净利润(同比)")
 	netprofitwith,
 	/**
 	 * 归属股东净利润
 	 */
+	@Description("归属股东净利润")
 	shnetprofit,
 	/**
 	 * 归属股东净利润(同比)
 	 */
+	@Description("归属股东净利润(同比)")
 	shnetprofitwith,
 	/**
 	 * 归属股东净利润(扣非)
 	 */
+	@Description("归属股东净利润(扣非)")
 	deshnetprofit,
 	/**
 	 * 扣非股东净利润(同比)
 	 */
+	@Description("扣非股东净利润(同比)")
 	deshnetprofitwith,
 	/**
 	 * 经营活动现金流量净额
 	 */
+	@Description("现金流量净额")
 	netcashflow,
 	/**
 	 * 经营活动现金流量净额(同比)
 	 */
+	@Description("现金流量净额(同比)")
 	netcashflowwith,
 	/**
 	 * 总资产
 	 */
+	@Description("总资产")
 	totalassets,
 	/**
 	 * 总资产(同比)
 	 */
+	@Description("总资产(同比)")
 	totalassetswith,
 	/**
 	 * 总负债
 	 */
+	@Description("总负债")
 	totalliability,
 	/**
 	 * 总负债(同比)
 	 */
+	@Description("总负债(同比)")
 	totalliabilitywith,
 	/**
 	 * 股东权益
 	 */
+	@Description("股东权益")
 	shequity,
 	/**
 	 * 股东权益(同比)
 	 */
+	@Description("股东权益(同比)")
 	shequitywith,
 	/**
 	 * 股份总数
 	 */
+	@Description("股份总数")
 	totalshares,
 	/**
 	 * 股份总数(同比)
 	 */
+	@Description("股份总数(同比)")
 	totalshareswith,
 	/**
 	 * 每股收益
 	 */
+	@Description("每股收益")
 	pershare,
 	/**
 	 * 每股收益(同比)
 	 */
+	@Description("每股收益(同比)")
 	persharewith,
 	/**
 	 * 资产周转率
 	 */
+	@Description("资产周转率")
 	assetturnover,
 	/**
 	 * 毛利率
 	 */
+	@Description("毛利率")
 	grossmargin,
 	/**
 	 * 净利率
 	 */
+	@Description("净利率")
 	netinterestrate,
 	/**
 	 * 主营业务利润占比
 	 */
+	@Description("主营业务利润占比")
 	mainprofitrate,
 	/**
 	 * 负债率
 	 */
+	@Description("负债率")
 	debtratio,
 	/**
 	 * 每股净资产
 	 */
+	@Description("每股净资产")
 	netassetspershare,
 	/**
 	 * 总资产收益率
 	 */
+	@Description("总资产收益率")
 	totalassetsprofitrate,
 	/**
 	 * 净资产收益率
 	 */
+	@Description("净资产收益率")
 	netAssetsProfitRate,
 	/**
 	 * 每股现金净额
 	 */
+	@Description("每股现金净额")
 	persharecash
 }
